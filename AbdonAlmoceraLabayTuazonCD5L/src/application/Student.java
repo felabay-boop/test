20b7fc45a65ff91b59b5eb0e5c24130392c911de
uncooked-noodle <abdon.lorraine.0004@gmail.com>
2025-11-21 21:43:18 +0800

    Added constructor and methods to Students class
    Implemented constructor and methods for the Students class.

--- AbdonAlmoceraLabayTuazonCD5L/src/application/Student.java
+++ AbdonAlmoceraLabayTuazonCD5L/src/application/Student.java
@@ -3,6 +3,7 @@ package application;
 import java.util.ArrayList;
 
 public class Students {
+	//attributes
 	private String name;
 	private int ID;
 	private ArrayList<Courses> courses;
@@ -14,8 +15,68 @@ public class Students {
 	ArrayList<Courses> courses,
 	DegProg degprog,
 	Account account) {
+		this.name = name;
+		this.ID = ID;
+		//courses - i forgot how to array wait
+		this.degprog = degprog;
+		this.account = account;
+	}
+
+	//getters for account display and boolean (ex. if degprog aligns with course)
+	public getName(){
+		return this.name;
+	}
+	public getID(){
+		return this.ID;
+	}
+	public getDegprog(){
+		return this.degree.name;
+	}
+
+	//setters
+		//for registering new accounts?
+	setName(){}
+	setID(){}
+	setDegProg(){}
+
+	
+	//methods
+	addCourse(){
 		
 	}
+	deleteCourse(){
+		
+	}
+	editCourse(){
+		
+	}
+
+	private doesAccExist(){ //boolean to check if account exists yet
+		if (this.account == NULL){
+			return false;
+		} else {
+			return true;
+		}
+	}
+	
+	login(){
+		//log in details and check if student already exists in system
+		if (doesAccExist() == true){
+			//start code for logging in
+		} else {
+			//prompt to register new account using register()
+		}
+		
+	}
+	register(){
+		//setter methods(?) to input account details
+	}
+	public viewWeeklyCalendar(){
+		
+	}
+	
+	
 }
+
 
 
