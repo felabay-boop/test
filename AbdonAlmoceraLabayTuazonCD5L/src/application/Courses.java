256c74c64a6917619d86fa8cafac0d44ab25c514
felabay-boop <felabay@up.edu.ph>
2025-11-21 21:22:13 +0800

    Update Courses.java

--- AbdonAlmoceraLabayTuazonCD5L/src/application/Courses.java
+++ AbdonAlmoceraLabayTuazonCD5L/src/application/Courses.java
@@ -1,5 +1,25 @@
 package application;
 
 public class Courses {
-	
+	private String courseCode; 
+	private String courseName;
+	private int units; 
+	private String room; 
+	private String[] days; 
+	private String section; 
+	private String startTime; 
+	private String endTime; 
+
+	public Courses (String courseCode, String courseName, int units
+				   , String room, String section, String startTime, 
+					String endTime){
+		this.courseCode = courseCode; 
+		this.courseName = courseName; 
+		this.units = units; 
+		this.room = room; 
+		this.section = section; 
+		this.startTime = startTime; 
+		this.endTime = endTime; 
+	}
 }
+
