9bab827a7a7c9a6142bdabc4f8f0f0fa9c82941d
uncooked-noodle <abdon.lorraine.0004@gmail.com>
2025-11-21 22:10:10 +0800

    Refactor Courses constructor and getter methods

--- AbdonAlmoceraLabayTuazonCD5L/src/application/Courses.java
+++ AbdonAlmoceraLabayTuazonCD5L/src/application/Courses.java
@@ -1,6 +1,7 @@
 package application;
 
 public class Courses {
+	//attributes
 	private String courseCode; 
 	private String courseTitle;
 	private int units;
@@ -9,9 +10,8 @@ public class Courses {
 	private String days; 
 	private String room; 
 
-	public Courses (String courseCode, String courseTitle, int units
-				   , String section, String time, String days, 
-					String room){
+	public Courses (String courseCode, String courseTitle, int units,
+					String section, String time, String days, String room){
 		this.courseCode = courseCode; 
 		this.courseTitle = courseTitle; 
 		this.units = units; 
@@ -22,10 +22,18 @@ public class Courses {
 	}
 
 	//getters:
-	public String getCourseCode (){ return courseCode }
-	public String getCourseTitle (){ return courseTitle }
-	public int getUnits (){ return units }
-	public String getSection (){ return section }
+	public String getCourseCode (){
+		return this.courseCode; 
+	}
+	public String getCourseTitle (){
+		return this.courseTitle;
+	}
+	public int getUnits (){
+		return this.units;
+	}
+	public String getSection (){
+		return this.section;
+	}
 
 	public void viewCourseDetails() {
 		//insert implementation
@@ -35,6 +43,7 @@ public class Courses {
 		//checks if a course is in a curriculum
 	}
 }
+
 
 
 
