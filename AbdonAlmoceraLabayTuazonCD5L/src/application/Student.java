5539b4c386bc681839fa0fc0c2729bf32911a9bd
aurumeleven <auxitrius786@gmail.com>
2025-12-01 13:46:04 +0800

    calendar stuff

--- AbdonAlmoceraLabayTuazonCD5L/src/application/Student.java
+++ AbdonAlmoceraLabayTuazonCD5L/src/application/Student.java
@@ -1,104 +1,79 @@
 package application;
 
+import java.time.LocalTime;
 import java.util.ArrayList;
+import java.util.Arrays;
 
-public class Student {
+public class Student implements Data{
 	//attributes
 	private String name;
 	private int ID;
-	private ArrayList<Courses> courses = new ArrayList<>();
+	private ArrayList<Course> courses = new ArrayList<>();
 	private String degprog;
-	private ArrayList<ArrayList<LocalTime>> calendar;
+	private ArrayList<ArrayList<LocalTime>> calendar = new ArrayList<>();
+	private String password;
 	
-	public Student(String name, int ID, ArrayList<Courses> courses, String degprog) {
+	public Student(String name, int ID, ArrayList<Course> courses, String degprog, String password) {
 		this.name = name;
 		this.ID = ID;
-		//courses - i forgot how to array wait
 		this.courses = courses;
 		this.degprog = degprog;
+		this.password = password;
 	}
 
 	//getters for account display and boolean (ex. if degprog aligns with course)
 	public String getName() {return this.name;}
 	public int getID() {return this.ID;}
-	public ArrayList<Courses> getCourses() {return this.courses;}
+	public ArrayList<Course> getCourses() {return this.courses;}
 	public String getDegprog() {return this.degprog;}
-
-	//setters
-		//for registering new accounts?
-//	setName(){}
-//	setID(){}
-//	setDegProg(){}
-	//we dont need them a student wont change their name or id or degree after creation
+	public String getPassword() {return this.password;}
 	
 	//methods
-	public void addCourse(Courses course){
-		for(Courses c : this.courses) {//loops through each course
+	public void addCourse(Course course){
+		
+		for(Course c : this.courses) {//loops through each course
 			if(c.getCourseCode().equals(course.getCourseCode())) {//checks if student already has course
 				System.out.println("ERROR you already have this course");
 				return;
 			}
-			for(String day : course.getDays()){
-				if(((course.getStartTime() > c.getStartTime() && course.getStartTime() < c.getEndTime()) ||
-				   (course.getEndTime() > c.getStartTime() && course.getEndTime() < c.getEndTime()) ||
-				   (course.getStartTime() < c.getStartTime() && course.getEndTime() > c.getEndTime())) && 
-				   (c.getDays().contains(day)){//if true, the course's schedule to be added collides with an existing course's
-					System.out.println("ERROR schedule collision with " + c.getName());
-					return;
+		for(String day : course.getDays()){
+			if (course.getStartTime().isBefore(c.getEndTime()) &&
+				course.getEndTime().isAfter(c.getStartTime()) && 
+			   (c.getDays().contains(day))){//if true, the course's schedule to be added collides with an existing course's
+				System.out.println("ERROR schedule collision with " + c.getCourseTitle());
+				return;
 				}
 			}
 		}
 		//goes here if no match
 		this.courses.add(course);
-		int dayIndex;
+		int dayIndex = -1;
 		for(String day : course.getDays()){
 			switch(day){
-				case "Monday":
-					dayIndex = 0;
-					break;
-				case "Tuesday":
-					dayIndex = 1;
-					break;
-				case "Wednesday":
-					dayIndex = 2;
-					break;
-				case "Thursday":
-					dayIndex = 3;
-					break;
-				case "Friday":
-					dayIndex = 4;
-					break;
+				case MONDAY:	dayIndex = 0;	break;
+				case TUESDAY:	dayIndex = 1;	break;
+				case WEDNESDAY:	dayIndex = 2;	break;
+				case THURSDAY:	dayIndex = 3;	break;
+				case FRIDAY:	dayIndex = 4;	break;
 			}
-			this.calendar.get(dayIndex).add(course.getStartTime());
-			this.calendar.get(dayIndex).add(course.getEndTime());
+			this.calendar.get(dayIndex).addAll(Arrays.asList(course.getStartTime(),course.getEndTime()));
 		}
 		System.out.println("SUCCESS course added");
 	}
-	public void deleteCourse(Courses course){
-		for(Courses c : this.courses) {//checks if course exists
-			if(c.getCourseCode().equals(course.getCourseCode())) {
+	public void deleteCourse(Course course){
+		for(Course c : this.courses) {//checks if course exists
+			if(c.getCourseCode().equals(course.getCourseCode())) {//course exists
 				this.courses.remove(c);
-				for(String day : course.getDays()){
-					int dayIndex;
+				for(String day : course.getDays()){//loops through each day course has to remove all times
+					int dayIndex = -1;
 						switch(day){
-					case "Monday":
-						dayIndex = 0;
-						break;
-					case "Tuesday":
-						dayIndex = 1;
-						break;
-					case "Wednesday":
-						dayIndex = 2;
-						break;
-					case "Thursday":
-						dayIndex = 3;
-						break;
-					case "Friday":
-						dayIndex = 4;
-						break;
+					case MONDAY:	dayIndex = 0;	break;
+					case TUESDAY:	dayIndex = 1;	break;
+					case WEDNESDAY:	dayIndex = 2;	break;
+					case THURSDAY:	dayIndex = 3;	break;
+					case FRIDAY:	dayIndex = 4;	break;
 					}
-					this.calendar.get(dayIndex).remove(course.getStartTime());
-					this.calendar.get(dayIndex).remove(course.getEndTime());
+						this.calendar.get(dayIndex).removeAll(Arrays.asList(course.getStartTime(),course.getEndTime()));
 				}
 				System.out.println("SUCCESS course removed");
 				return;
@@ -107,30 +82,7 @@ public class Student {
 		//goes here if no match
 		System.out.println("ERROR you dont have this course");
 	}
-//	editCourse(){
-//		
-//	}do we need this
 
-//	private doesAccExist(){ //boolean to check if account exists yet
-//		if (this.account == NULL){
-//			return false;
-//		} else {
-//			return true;
-//		}
-//	}
-	
-//	login(){should be on welcome i think
-//		//log in details and check if student already exists in system
-//		if (doesAccExist() == true){
-//			//start code for logging in
-//		} else {
-//			//prompt to register new account using register()
-//		}
-//		
-//	}
-//	register(){should be on welcome i think
-//		//setter methods(?) to input account details
-//	}
 	public void viewWeeklyCalendar(){
 		
 	}
