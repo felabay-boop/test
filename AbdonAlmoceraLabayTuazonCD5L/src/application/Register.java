1d4c907e1be4bc8ef6c9846c4c3dbbf40b953b02
aurumeleven <auxitrius786@gmail.com>
2025-12-04 19:08:53 +0800

    created login and register dialog boxes
    add the code to start the dashboard in here it is inside login

--- AbdonAlmoceraLabayTuazonCD5L/src/application/Register.java
+++ AbdonAlmoceraLabayTuazonCD5L/src/application/Register.java
@@ -1,98 +1,144 @@
-public class Register {
-  private Scene scene;
-	private GridPane root;
-	
-	TextField username = new TextField();
-	TextField email = new TextField();
-	TextField first = new TextField();
-	TextField middle = new TextField();
-	TextField last = new TextField();
-	ComboBox<String> usertype = new ComboBox<String>();
-	PasswordField password = new PasswordField();
-	
-	Text error = new Text("All textboxes \n(except the middle name) \nshould be filled up!!");
-	
-	public Create_New_User() {
-		this.root = new GridPane();
-		this.scene = new Scene(root, 300, 600);
-	}
-	
-	public void start(Stage stage, Main main) {
-		Text username_t = new Text("Username:");
-		Text email_t = new Text("Email Address");
-		Text first_t = new Text("First Name:");
-		Text middle_t = new Text("Middle Name");
-		Text last_t = new Text("Last Name");
-		Text usertype_t = new Text("User Type");
-		Text password_t = new Text("Password:");
-		
-		Button create = new Button("Login");
-    Button cancel = new Button("Cancel");
-    
-    usertype.getItems().addAll("PROFESSOR", "STUDENT", "PREFECT");
-    
-    create.setOnMouseClicked(new EventHandler<MouseEvent>() {//create button function
-			public void handle(MouseEvent arg0) {
-				if(username.getCharacters().toString().equals("")
-				|| email.getCharacters().toString().equals("")
-				|| first.getCharacters().toString().equals("")
-				|| last.getCharacters().toString().equals("")
-				|| usertype.getValue() == null
-				|| password.getCharacters().toString().equals("")) {
-					
-					root.add(error, 0, 7);
-					stage.setScene(scene);
-					stage.show();
-				}
-				else {
-					main.sampleList.add(new User(username.getCharacters().toString(),
-							email.getCharacters().toString(),
-							first.getCharacters().toString(),
-							middle.getCharacters().toString(),
-							last.getCharacters().toString(),
-							usertype.getValue(),
-							password.getCharacters().toString()));
-					
-					ObservableList<User> data = FXCollections.observableArrayList(main.sampleList);
-			        main.table.setItems(data);
-			        stage.close();
-				}
-			}
-		});
-        
-        cancel.setOnMouseClicked(new EventHandler<MouseEvent>() {
-			public void handle(MouseEvent arg0) {
-				stage.close();
-			}
-		});
-        
-        HBox hbox = new HBox();
-		root.setStyle("-fx-padding: 12;");
-		
-		hbox.getChildren().add(create);
-		hbox.getChildren().add(cancel);
-		
-		this.root.add(username_t, 0, 0);
-		this.root.add(email_t, 0, 1);
-		this.root.add(first_t, 0, 2);
-		this.root.add(middle_t, 0, 3);
-		this.root.add(last_t, 0, 4);
-		this.root.add(usertype_t, 0, 5);
-		this.root.add(password_t, 0, 6);
-		
-		this.root.add(username, 1, 0);
-		this.root.add(email, 1, 1);
-		this.root.add(first, 1, 2);
-		this.root.add(middle, 1, 3);
-		this.root.add(last, 1, 4);
-		this.root.add(usertype, 1, 5);
-		this.root.add(password, 1, 6);
-		
-		this.root.add(hbox, 1, 7);
-		
-		stage.setTitle("Create New User");
-		stage.setScene(this.scene);
-		stage.show();
-	}
+package application;
 
+import java.util.ArrayList;
+
+import javafx.beans.value.ChangeListener;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Dialog;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ButtonBar;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Text;
+
+public class Register implements Data{//class for registering and holding student data
+	public static ArrayList<Student> STUDENTS = new ArrayList<>();
+	public static void addStudent(Student s) {STUDENTS.add(s);}
+	
+	public static void loginScreen() {
+		//creates dialog box for login and register
+		Dialog<Void> loginDialog = new Dialog<Void>();
+		GridPane loginDetails = new GridPane();
+		
+		//creates content for gridpane
+		Text usertext = new Text("Username:");
+		Text passtext = new Text("Password:");
+		TextField userinput = new TextField();
+		PasswordField passinput = new PasswordField();
+		Text orRegister = new Text("or make a new account: ");
+		Button registerButton = new Button("Register");
+		
+		//adds content to gridpane
+		loginDetails.add(usertext, 0, 0);
+		loginDetails.add(passtext, 0, 1);
+		loginDetails.add(userinput, 1, 0);
+		loginDetails.add(passinput, 1, 1);
+		loginDetails.add(orRegister, 0, 2);
+		loginDetails.add(registerButton, 1, 2);
+		
+		//adds ok/confirm button for login
+		ButtonType okType = new ButtonType("OK", ButtonBar.ButtonData.OK_DONE);
+		loginDialog.getDialogPane().getButtonTypes().add(okType);
+		loginDialog.getDialogPane().setContent(loginDetails);
+		//changes ok button's text to "Login"
+		Button okButton = (Button) loginDialog.getDialogPane().lookupButton(okType);
+		okButton.setText("Login");
+		
+		//ensures login input fields are required
+		okButton.setDisable(true);
+		ChangeListener<String> loginListener = (n,m,input) -> {
+			if(!userinput.getText().isEmpty() && !passinput.getText().isEmpty())	okButton.setDisable(false);
+			else okButton.setDisable(true);
+		};
+		userinput.textProperty().addListener(loginListener);
+		passinput.textProperty().addListener(loginListener);
+		
+		//event handler for login button
+		okButton.addEventFilter(ActionEvent.ACTION, event -> {
+			event.consume();//doesnt close immediately
+				for(Student s : STUDENTS) {
+					if(s.getUsername().equals(userinput.getText()) && s.getPassword().equals(passinput.getText())) {//login successful
+						//dashboard starting method here!!!
+						System.out.println("VALID");
+						return;
+					}
+				}
+				//goes here if login unsuccessful
+				System.out.println("INVALID");
+				Alert invalidlogin = new Alert(AlertType.ERROR);
+				invalidlogin.setContentText("Invalid username and password.");
+				invalidlogin.showAndWait();
+		});
+		
+		//event handler for register button
+		registerButton.setOnAction(event ->{registerScreen();});
+				
+		loginDialog.showAndWait();
+	}
+	
+	private static void registerScreen() {
+		Dialog<Void> registerDialog = new Dialog<Void>();
+		GridPane registerDetails = new GridPane();
+		
+		//creates content for gridpane
+		Text usertext = new Text("Username:");
+		Text nametext = new Text("Name:");
+		Text IDtext = new Text("ID:");
+		Text degreetext = new Text("Degree Program:");
+		Text passtext = new Text("Password:");
+		TextField userinput = new TextField();
+		TextField nameinput = new TextField();
+		TextField IDinput = new TextField();
+		ComboBox<String> degreeinput = new ComboBox<String>();
+		degreeinput.getItems().addAll(CMSC,MSCS,MIT,PHD);
+		degreeinput.setValue(CMSC);
+		PasswordField passinput = new PasswordField();
+		
+		//adds content to gridpane
+		registerDetails.add(usertext, 0, 0);
+		registerDetails.add(nametext, 0, 1);
+		registerDetails.add(IDtext, 0, 2);
+		registerDetails.add(degreetext, 0, 3);
+		registerDetails.add(passtext, 0, 4);
+		registerDetails.add(userinput, 1, 0);
+		registerDetails.add(nameinput, 1, 1);
+		registerDetails.add(IDinput, 1, 2);
+		registerDetails.add(degreeinput, 1, 3);
+		registerDetails.add(passinput, 1, 4);
+		
+		//adds ok/confirm and cancel buttons for register
+		ButtonType okType = new ButtonType("OK", ButtonBar.ButtonData.OK_DONE);
+		ButtonType cancelType = new ButtonType("Cancel", ButtonBar.ButtonData.CANCEL_CLOSE);
+		registerDialog.getDialogPane().getButtonTypes().addAll(okType,cancelType);
+		registerDialog.getDialogPane().setContent(registerDetails);
+		//changes ok button's text to "Register"
+		Button okButton = (Button) registerDialog.getDialogPane().lookupButton(okType);
+		okButton.setText("Register");
+		
+		//ensures register input fields are required
+		okButton.setDisable(true);
+		ChangeListener<String> registerListener = (n,m,input) -> {
+			if(!userinput.getText().isEmpty() && !nameinput.getText().isEmpty() && !IDinput.getText().isEmpty() && !passinput.getText().isEmpty())	okButton.setDisable(false);
+			else okButton.setDisable(true);
+		};
+		userinput.textProperty().addListener(registerListener);
+		nameinput.textProperty().addListener(registerListener);
+		IDinput.textProperty().addListener(registerListener);
+		passinput.textProperty().addListener(registerListener);
+		
+		//event handler for login button
+		okButton.addEventFilter(ActionEvent.ACTION, event -> {
+			event.consume();//doesnt immediately close
+			//creates new student given registration details
+			STUDENTS.add(new Student(nameinput.getText(),IDinput.getText(),new ArrayList<>(),degreeinput.getValue(),userinput.getText(),passinput.getText()));
+			System.out.println("VALID");
+			registerDialog.close();
+		});
+		registerDialog.showAndWait();
+	}
 }
